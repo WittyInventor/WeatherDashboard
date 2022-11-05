c66c472a30c4370ffb113621d19dd5e26f19110e
Annemarie Sheridan <atsheridan38@gmail.com>
2022-11-04 18:06:38 -0700

    updated code

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+var searchBox = document.querySelector('#searchBox');
+
+var weatherButton = document.querySelector('#weatherButton');
+
+var cityName = document.querySelector('.city');
+
+weatherButton.addEventListener('click', function(){
+    console.log("weatherButton")
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchBox.value}&appid=961c08e4a008934a3cf03bcaca4ac1f3`)
+    .then(function(response) {/Users/annemariesheridan/Desktop/bootcamp/challenges/WeatherDashboard/Assets/06-server-side-apis-homework-demo.png
+    // console.log(response)
+    if (!response.ok){
+        throw response.json();
+    }
+    return response.json();
+    }).then(function(weatherInfo){
+    console.log(weatherInfo)
+    console.log(weatherInfo.city.name)
+
+    cityName.innerHTML=weatherInfo.city.name 
+
+    })
+
+});
+
+
+
+
+var bermuda = {
+    name: "leroy",
+    food: "turkey",
+    carabou: true
+}
+console.log(bermuda.food)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WeatherDashboard
+
+pro of JS dynamic HTML generation
+less styling upfont, but more after
+edit html dynamically, or change HTML conditionally
+
+cons of JS dynamic HTML generation
+fells less fluid
