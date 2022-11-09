d8e7c0a8a277c7f3ba820a89d86bd5b83c701b4b
Annemarie Sheridan <atsheridan38@gmail.com>
2022-11-09 12:48:08 -0800

    updated java and html

--- Assets/script.js
+++ Assets/script.js
@@ -29,17 +29,23 @@ weatherButton.addEventListener('click', function(){
     console.log(weatherInfo)
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main)
     // this list.[0] is referring to the first index of the list array in the console log that shows the temperature and other weather info.
+
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].wind.speed)
 
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main.humidity)
 
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main)
 
-    cityName.innerHTML=weatherInfo.city.name 
+    let date = new Date(weatherInfo.list[0].dt * 1000)
+    //  dt value means  milliseconds since Jan 1st 1970. It is multiplied(*) by 1000 to properly indicate the correct date.
+
+    cityName.innerHTML=weatherInfo.city.name + "  " + (date.getMonth() + 1) + "/" + date.getDate()
 
     tempCity.innerHTML= "Temp: " + weatherInfo.list[0].main.temp + " F"
 
     windCity.innerHTML= "Wind: " + weatherInfo.list[0].wind.speed + " mph"
+
+    document.querySelector("icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
     humidCity.innerHTML= "Humidity: " + weatherInfo.list[0].main.humidity + " mph"
     })
