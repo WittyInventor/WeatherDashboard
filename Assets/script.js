7d223dce5cedf55d3ae2b116e228e6f19e9eb2cc
Annemarie Sheridan <atsheridan38@gmail.com>
2022-11-10 11:23:54 -0800

    updated part 1 of five day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,7 @@ var windCity = document.querySelector('.wind');
 
 var humidCity = document.querySelector('.humidity');
 
-var rainCity = document.querySelector('.rainfall');
+
 
 weatherButton.addEventListener('click', function(){
     console.log("weatherButton")
@@ -27,14 +27,15 @@ weatherButton.addEventListener('click', function(){
     return response.json();
     }).then(function(weatherInfo){
     console.log(weatherInfo)
-    console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main)
+    console.log("WEATHER INFO LIST 0",weatherInfo)
     // this list.[0] is referring to the first index of the list array in the console log that shows the temperature and other weather info.
 
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].wind.speed)
 
     console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main.humidity)
 
-    console.log("WEATHER INFO LIST 0",weatherInfo.list[0].main)
+    console.log("WEATHER INFO",weatherInfo.list[0].weather[0].icon)
+    document.querySelector(".icon").setAttribute("src","https://openweathermap.org/img/wn/" + weatherInfo.list[0].weather[0].icon + "@2x.png")
 
     let date = new Date(weatherInfo.list[0].dt * 1000)
     //  dt value means  milliseconds since Jan 1st 1970. It is multiplied(*) by 1000 to properly indicate the correct date.
@@ -45,12 +46,29 @@ weatherButton.addEventListener('click', function(){
 
     windCity.innerHTML= "Wind: " + weatherInfo.list[0].wind.speed + " mph"
 
-    document.querySelector("icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    // document.querySelector(".icon").setAttribute("src","https://openweathermap.org/img/wn/" + + "@2x.png")
+    
 
     humidCity.innerHTML= "Humidity: " + weatherInfo.list[0].main.humidity + " mph"
     })
     })
 
+    // Below is the section organizing the future first day forecast from the current date's posted.
+
+    var date1 = document.querySelector('.date1');
+
+    var temp1 = document.querySelector('.temp1');
+
+    temp1.innerHTML= "Temp: " + weatherInfo.list[1].main.temp1 + " F"
+    
+    var wind1 = document.querySelector('.wind1');
+    
+    var humid1 = document.querySelector('.humid1');
+
+    var icon1 = document.querySelector('.icon1');
+
+    cityName.innerHTML=weatherInfo.city.name + "  " + (date.getMonth() + 1) + "/" + date.getDate()
+    // im confused if this is right for the date showing for the future forecast..
 
 
 
